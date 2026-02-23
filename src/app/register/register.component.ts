85d06a1aace845f122ef2836a988d61f72d64f5e
mark lim <mblim1220@gmail.com>
2026-02-23 16:46:00 +0800

    Add dark theme toggle and enhance form validation in registration component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,6 +8,9 @@ import { MatFormField } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-register',
@@ -22,7 +25,10 @@ import { MatSliderModule } from '@angular/material/slider';
     DatePipe,
     MatInputModule,
     MatRadioModule,
-    MatSliderModule
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatIconModule,
+    MatCardModule
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
@@ -39,16 +45,40 @@ export class RegisterComponent {
   submitted = false;
   minSkilllevel = 1;
   maxSkilllevel = 10;
+  isDarkMode = false;
 
   formdata: FormGroup = new FormGroup({
     userName: new FormControl(''),
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.pattern(/^[a-zA-Z][a-zA-Z0-9]*$/)
+    ]),
     gender: new FormControl('', [Validators.required]),
-    birthDate: new FormControl(null, [Validators.required]),
+    birthDate: new FormControl(null, [Validators.required, this.birthYearValidator]),
     address: new FormControl(''),
     angularSkilllevel: new FormControl(5)
   });
+
+  birthYearValidator(control: import('@angular/forms').AbstractControl) {
+    if (control.value) {
+      const year = new Date(control.value).getFullYear();
+      if (year > 2006) {
+        return { invalidYear: true };
+      }
+    }
+    return null;
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      document.body.classList.add('dark-theme');
+    } else {
+      document.body.classList.remove('dark-theme');
+    }
+  }
 
   onSubmit() {
     this.submitted = true;
